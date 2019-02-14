131cded87661f1f9d458017e6e15e17548fa65fe
jdheyburn <heyjoeburn@gmail.com>
2019-02-14 09:17:50 +0000

    Fix netlify build

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -3,6 +3,7 @@
 # https://github.com/netlify/build-image/issues/183#issuecomment-419199649
 
 HUGO_FLAVOUR="hugo_extended"
+HUGO_VERSION="0.54.0"
 
 rm -rf *.deb
 wget --no-clobber http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb || exit 1
@@ -14,4 +15,3 @@ rm -rf *.deb
 export LD_LIBRARY_PATH=$(pwd)/tmp/usr/lib/x86_64-linux-gnu
 export PATH=$(pwd)/tmp/usr/local/bin:$PATH
 
-hugo
